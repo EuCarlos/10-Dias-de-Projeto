7e79f754c137c347de7512b4814a2cb6a3eac6c9
Carlos Alves <carlosalves369m@hotmail.com>
2020-09-29 14:44:08 -0300

    10°Dia

--- Gerador de Certificados/GeradorDeCertificado.py
+++ Gerador de Certificados/GeradorDeCertificado.py
@@ -1,24 +1,30 @@
+# encoding: utf-8
 from tkinter import *
 from openpyxl import load_workbook
 from datetime import date
-#Criar um Entry para o "Nome do Curso"
-#Criar uma def para salvar como PDF
-'''
-# Fazer o programa ler o arquivo .txt(com nomes do anulos)
-# salvar o arquivo em um mainloop
-    for i in range(len('nome_da_variavel_dos_Aqruivo))
-        pegar nomes[i]
-        planilha.cell(row=linhaQueONomeVaiFicar, column=ColunadoDoNome, value=pegar nome[i])
-        salvaOArquivo('Certificado{}.txt'.format(i))
-'''
+from converterArq import Excel2Pdf, apagaArq
 
-def getValores():
-    nome = FNome.get()
-    prof = FProf.get()
-    planilha1.cell(row=7,column=5, value=nome)
-    planilha1.cell(row=26, column=2, value=date)
-    planilha1.cell(row=26, column=11, value=prof)
-    arquivo_excel.save("Certificado.xlsx")
+arq = open('NomeDosAlunos.txt')
+alunos = arq.readlines()
+
+def gerarCertificado():
+
+    for i in range(len(alunos)):
+        prof = FProf.get()
+        planilha1.cell(row=13, column=2, value=alunos[i])
+        planilha1.cell(row=19, column=5, value=date)
+        planilha1.cell(row=26, column=2, value=prof)
+        arquivo_excel.save('Resultados/Certificado{}.xlsx'.format(i))
+        txt4 = Label(jan, text='CERTIFICADOS GERADOS', bg='green', fg='white').place(x=90, y=160)
+
+    for i in range(len(alunos)):
+        input_file = r'C:\[caminho_do_arquivo]\Resultados\Certificado{}.xlsx'.format(i)
+        output_file = r'C:\[caminho_do_arquivo]\Resultados\Certificado{}.pdf'.format(i)
+        Excel2Pdf(input_file, output_file)
+
+    for i in range(len(alunos)):
+        input_file = r'C:\[caminho_do_arquivo]\Resultados\Certificado{}.xlsx'.format(i)
+        apagaArq(input_file)
 
 date_today = date.today()
 date = '{}/{}/{}'.format(date_today.day, date_today.month, date_today.year)
@@ -36,12 +42,10 @@ jan.geometry('300x200+500+300')
 jan.resizable(width=False, height=False)
 
 txt1 = Label(jan, text='GERAR CERTIFICADO', bg=cor1, fg='black').pack(side=TOP, fill=X)
-txt2 = Label(jan, text='NOME COMPLETO:', bg=corBG, fg='black').place(x=20, y=50)
-FNome = Entry(jan)
-FNome.place(x=155, y=50)
-txt3 = Label(jan, text='NOME DO PROFESSOR:', bg=corBG, fg='black').place(x=20, y=85)
+txt2 = Label(jan, text='DATA DOS CERTIFICADOS: '+ date, bg=corBG, fg='black').place(x=20, y=50)
+txt3 = Label(jan, text='NOME DO INTRUTOR:', bg=corBG, fg='black').place(x=20, y=85)
 FProf = Entry(jan)
 FProf.place(x=155, y=85)
-btnGerar = Button(jan, width='20', text='GERAR CERTIFICADO', fg='white', bg=cor1, command=getValores).place(x=85, y=120)
+btnGerar = Button(jan, width='20', text='GERAR CERTIFICADO', fg='white', bg=cor1, command=gerarCertificado).place(x=85, y=120)
 
 jan.mainloop()
